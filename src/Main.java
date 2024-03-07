21dc990fcd3c8a64bc1ec833876c7f4656c04b72
tmarczyn <01161952@pw.edu.pl>
2024-03-07 22:50:29 +0100

    Zadanie 3

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int randomNumber=(int)(Math.random()*100);
+        System.out.println("Random number: "+randomNumber);
+        if(randomNumber%2==0)
+            System.out.println("Jest parzysta");
+        else{
+            System.out.println("Nie jest parzysta");
+        }
+
+
+    }
+}
